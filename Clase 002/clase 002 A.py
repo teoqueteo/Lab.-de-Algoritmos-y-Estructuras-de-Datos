f8c915660ed9d4ae16ebd28cf722bf117e82ef66
teoqueteo <teoqueteo@gmail.com>
2025-03-12 11:44:46 -0300

    Add files via upload

---
+++ Clase 002/clase 002 A.py
@@ -0,0 +1,7 @@
+cuadro = ""
+cuadro = input("¿Cuál es el más grande?: ").strip().upper()
+while cuadro != "RIVER":
+    print("Nuh bro, try again gil")
+    print("")
+    cuadro = input("¿Cuál es el más grande?: ").strip().upper()
+print("Ahí va crack, vamo el más grande")
