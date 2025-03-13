54b9a336da969a407554dc8281d630f830a6f47a
teoqueteo <teoqueteo@gmail.com>
2025-03-13 13:44:23 -0300

    Delete Clase 002 directory

--- Clase 002/clase 002 A.py
+++
@@ -1,7 +0,0 @@
-cuadro = ""
-cuadro = input("¿Cuál es el más grande?: ").strip().upper()
-while cuadro != "RIVER":
-    print("Nuh bro, try again gil")
-    print("")
-    cuadro = input("¿Cuál es el más grande?: ").strip().upper()
-print("Ahí va crack, vamo el más grande")
--- Clase 002/clase 002 B.py
+++
@@ -1,3 +0,0 @@
-nombre =input("Ingresa tu nombre: ").strip().lower()
-apellido =input("Ingresa tu apellido: ").strip().lower()
-print  ("Buenos dias " f"{nombre} {apellido}")
