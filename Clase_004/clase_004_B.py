cb0ce3ddacfa16d214a40f8685ece9658fa81afa
teoqueteo <teoqueteo@gmail.com>
2025-03-19 11:54:35 -0300

    Add files via upload

---
+++ Clase_004/clase_004_B.py
@@ -0,0 +1,5 @@
+bicis = ['BMX', 'urbana', 'plegable', 'de montaña']
+print(bicis)         # Imprime toda la lista
+print(bicis[0])      # Imprime el primer elemento de la lista ('BMX')
+print(bicis[0].lower())  # Convierte 'BMX' a minúsculas y la imprime
+print(bicis[-1])     # Imprime el último elemento de la lista ('de montaña')
