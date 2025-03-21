e935dc59cdf60e48df2255beae169e5e26efe574
teoqueteo <teoqueteo@gmail.com>
2025-03-21 14:56:56 -0300

    Add files via upload

---
+++ Clase_005/clase_005_011.py
@@ -0,0 +1,3 @@
+num = list (range (1,1000001))
+print(f" De {min(num)} a {max(num)} \n")
+print(sum(num))
---
+++ Clase_005/clase_005_013.2.py
@@ -0,0 +1,11 @@
+color_alien = ('rojo')
+if color_alien == ('verde'):
+    print(f"El jugador acaba de ganar 5 puntos.")
+else:
+    print(f"El jugador acaba de ganar 10 puntos.")
+
+color_alien = ('verde')
+if color_alien == ('verde'):
+    print(f"El jugador acaba de ganar 5 puntos.")
+else:
+    print(f"El jugador acaba de ganar 10 puntos.")
