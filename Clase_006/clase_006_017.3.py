0faaaa09c209d7ce71e237688fb48efad19ebe3e
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:06:25 -0300

    Delete Clase_006/clase_006_017.3.py

--- Clase_006/clase_006_017.3.py
+++
@@ -1,7 +0,0 @@
-lugares_favoritos = {
-    'Juan': ['Paris', 'Roma', 'Tokio'],
-    'Sara': ['Barcelona', 'Londres', 'Bali'],
-    'Eduardo': ['Nueva York', 'Los Ángeles', 'Chicago']}
-for persona, lugares in lugares_favoritos.items():
-    lugares_str = ', '.join(lugares)
-    print(f"{persona} tiene como lugares favoritos: {lugares_str}.")
