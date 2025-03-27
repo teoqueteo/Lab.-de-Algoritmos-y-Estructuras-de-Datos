d7eb1df73378de4ac049a1bbc71f145c1f7d4edd
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:08:38 -0300

    Delete Clase_006/clase_006_022.1.py

--- Clase_006/clase_006_022.1.py
+++
@@ -1,4 +0,0 @@
-def hacer_camiseta(tamano, mensaje):
-    print(f"Estás haciendo una camiseta de tamaño {tamano} con el mensaje: '{mensaje}'.")
-hacer_camiseta("M", "¡Python es increíble!")
-hacer_camiseta(tamano="L", mensaje="Aprende a programar con Python.")
