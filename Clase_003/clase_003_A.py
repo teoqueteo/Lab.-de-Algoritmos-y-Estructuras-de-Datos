64a991371f3f335191f4f647568bd4a398425bac
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:00:16 -0300

    Delete Clase_003/clase_003_A.py

--- Clase_003/clase_003_A.py
+++
@@ -1,3 +0,0 @@
-nombre = input("Tu nombre pa: ").capitalize()  # Devuelve una copia con la primera letra en mayúscula
-print("Buenos días", nombre)
-print("Tu nombre sin vocales es:", nombre.strip('aeiouAEIOU.')) # Usamos .strip() para eliminar las solamente las vocales de nombre
