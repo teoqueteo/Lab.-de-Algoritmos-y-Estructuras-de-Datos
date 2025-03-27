ca31cff126fe8d1db1785a5ad09c81c3d90a78a1
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:02:48 -0300

    Delete Clase_005/clase_005_013.1.py

--- Clase_005/clase_005_013.1.py
+++
@@ -1,7 +0,0 @@
-color_alien = ('rojo')
-if color_alien == ('verde'):
-    print(f"El jugador acaba de ganar 5 puntos.")
-
-color_alien = ('verde')
-if color_alien == ('verde'):
-    print(f"El jugador acaba de ganar 5 puntos.")
