bbca319af4e433de29bd123ff90d77915f5be2cc
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:06:04 -0300

    Delete Clase_006/clase_006_017.1.py

--- Clase_006/clase_006_017.1.py
+++
@@ -1,7 +0,0 @@
-persona_1 = {'Nombre': 'Sal', 'Apellido': 'Chicha', 'Edad': '70', 'Ciudad': 'La Paz'}
-persona_2 = {'Nombre': 'Ana', 'Apellido': 'Gomez', 'Edad': '28', 'Ciudad': 'Madrid'}
-persona_3 = {'Nombre': 'Luis', 'Apellido': 'Pérez', 'Edad': '34', 'Ciudad': 'Buenos Aires'}
-personas = [persona_1, persona_2, persona_3]
-for persona in personas:
-    for info, dato in persona.items():
-        print(f"{info}: {dato}. \n")
