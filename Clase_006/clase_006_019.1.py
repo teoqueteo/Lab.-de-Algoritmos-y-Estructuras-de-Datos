51a15089eb2ed0d7b23649ac2255261fdd6fe472
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:06:58 -0300

    Delete Clase_006/clase_006_019.1.py

--- Clase_006/clase_006_019.1.py
+++
@@ -1,5 +0,0 @@
-while True:
-    ingrediente = input("Ingresa un ingrediente para la pizza (o escribe 'salir' para terminar): ").lower().strip()
-    if ingrediente == 'salir':
-        break
-    print(f"Vas a agregar {ingrediente} a tu pizza.") 
