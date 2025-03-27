9d1300ed082f0e21cd3a0950b66ce09218704b12
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:02:38 -0300

    Delete Clase_005/clase_005_009.2.py

--- Clase_005/clase_005_009.2.py
+++
@@ -1,4 +0,0 @@
-invitados = ['Marcelote', 'Juan Carlos', 'Esternocleidomastoideo']
-for invitado in invitados:
-    print(f"Hola {invitado}, te invito a cenar. ¡Espero verte!")
-print(f"Se invitaron a {len(invitados)} personas.")
