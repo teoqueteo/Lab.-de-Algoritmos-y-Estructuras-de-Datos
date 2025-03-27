2e04f4f750e0ed11ecac5d7e8d0458c841f0a508
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:04:00 -0300

    Delete Clase_006/clase_006_014.3.py

--- Clase_006/clase_006_014.3.py
+++
@@ -1,8 +0,0 @@
-usuarios_actuales = ['admin','pepe','micho','marce','eduardo']
-usuarios_nuevos = ['admin','pepe','titi','gerardo','emilianio']
-for usuario_nuevo in usuarios_nuevos:
-    for usuario_actual in usuarios_actuales:
-        if usuario_nuevo == usuario_actual:
-            print(f"Nombre ya usado, elegí otro.")
-        else:
-            print(f"Nombre aceptado.")
