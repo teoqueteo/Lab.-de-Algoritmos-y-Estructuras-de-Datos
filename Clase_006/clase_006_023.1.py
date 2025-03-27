be83ef2154a7e1f1b767d199335e12f30f10ba25
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:10:04 -0300

    Delete Clase_006/clase_006_023.1.py

--- Clase_006/clase_006_023.1.py
+++
@@ -1,5 +0,0 @@
-def ciudad_pais(ciudad, pais):
-    return f"{ciudad}, {pais}"
-print(ciudad_pais("Santiago", "Chile"))
-print(ciudad_pais("Londres", "Reino Unido"))
-print(ciudad_pais("Tokyo", "Japón"))
