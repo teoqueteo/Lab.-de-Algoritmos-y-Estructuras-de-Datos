c54cc8baab1b33f6771d7c7387b78076277e622c
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:09:39 -0300

    Delete Clase_006/clase_006_022.3.py

--- Clase_006/clase_006_022.3.py
+++
@@ -1,5 +0,0 @@
-def describir_ciudad(ciudad, pais="Islandia"):
-    print(f"{ciudad} está en {pais}.")
-describir_ciudad("Reikiavik")
-describir_ciudad("Oslo", "Noruega") 
-describir_ciudad("Buenos Aires", "Argentina")
