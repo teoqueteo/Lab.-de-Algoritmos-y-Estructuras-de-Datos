b6f4d9226f20aafc5adb62defa052b11bfc898e4
teoqueteo <teoqueteo@gmail.com>
2025-03-27 15:03:52 -0300

    Delete Clase_006/clase_006_014.2.py

--- Clase_006/clase_006_014.2.py
+++
@@ -1,6 +0,0 @@
-usuarios = ['admin','pepe','micho','marce','eduardo']
-for usuario in usuarios:
-    if usuario == 'admin':
-        print(f"Hola admin.")
-    else:
-        print(f"Hola {usuario}, gracias por volver a iniciar sesión.")
